eaba31e2365fc68695780416ce4691272eec0c35
Albert Kison <albert.kison@student.griffith.ie>
2024-04-25 04:46:05 +0100

    Exercise 7

--- Assets/Scripts/PlayerNavMesh.cs
+++ Assets/Scripts/PlayerNavMesh.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerNavMesh : MonoBehaviour
 {
+    public Animator animator;
+    public NavMeshAgent agent;
+    public float velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //get the velocity of the player
+        velocity = agent.velocity.magnitude;
+        //set the speed of the animator to the velocity of the player
+        animator.SetFloat("velocity", velocity);
+
         // move player towards the mouse click using navmesh
         if (Input.GetMouseButtonDown(0))
         {
